52040271e76450e8e74a64fe53333e43e2837f3e
James Keesey <james.keesey@gmail.com>
2022-06-30 11:46:49 -0700

    Final (hopefully) touch ups for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,10 +15,12 @@
  *
  */
 
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.apache.tools.ant.taskdefs.condition.Os
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import pl.allegro.tech.build.axion.release.domain.hooks.HooksConfig
+import pl.allegro.tech.build.axion.release.domain.preRelease
 import java.io.FileNotFoundException
-import java.util.Properties
+import java.util.*
 
 @Suppress("DSL_SCOPE_VIOLATION", "UnstableApiUsage") plugins {
     kotlin("jvm")
@@ -28,13 +30,9 @@ import java.util.Properties
     alias(libs.plugins.gradleVersions)
     alias(libs.plugins.launch4j)
     alias(libs.plugins.axionRelease)
-//    alias(libs.plugins.release)
 }
+
 project.version = scmVersion.version
-
-ext.set("release.dryRun", true)
-
-val dryrunner: String by extra("release.dryRun")
 
 val applicationName: String by extra(rootProject.name)
 val applicationTitle: String by extra("Chatter XIV")
@@ -122,6 +120,9 @@ tasks {
         val authorization = loadProperties("release.properties")
         scmVersion.repository.customUsername = authorization["axion.username"] as String
         scmVersion.repository.customPassword = authorization["axion.password"] as String
+        if (project.hasProperty("releaseDryRun")) {
+            ext.set("release.dryRun", true)
+        }
     }
 
     named("release") {
@@ -133,6 +134,10 @@ application {
     mainClass.set(applicationMainClassName)
 }
 
+/**
+ * Creates installers for this product. Currently this only creates a Windows MSI installer.
+ * For Linux et al just user the tar file.
+ */
 runtime {
     options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
     modules.set(
@@ -163,26 +168,52 @@ runtime {
     }
 }
 
+/**
+ * Handles the versioning of the product. This creates several useful tasks. The most useful
+ * are currentVersion which returns the current version as this plugin knows it and release
+ * which makes the current code a new release by tagging the current commit and pushing
+ * the changes to the origin. See https://axion-release-plugin.readthedocs.io/en/latest/
+ */
 scmVersion {
     useHighestVersion = true
     versionIncrementer("incrementMinor")
-    dryRun = true
-//    hooks {
-//        preRelease {
-//            fileUpdate {
-//                file("README.md")
-//                pattern = { previousVersion, _ -> "Stable version $previousVersion" }
-//                replacement = { currentVersion, _ -> "Stable version $currentVersion" }
-//            }
-//            fileUpdate {
-//                file("README.md")
-//                pattern = { previousVersion, _ -> "Stable-$previousVersion-" }
-//                replacement = { currentVersion, _ -> "Stable-$currentVersion-" }
-//            }
-//        }
-//    }
+    hooks {
+        preRelease {
+            preFileUpdate(file = "README.md",
+                          pattern = { previousVersion, _ -> "Stable version $previousVersion" },
+                          replacement = { currentVersion, _ -> "Stable version $currentVersion" })
+            preFileUpdate(file = "README.md",
+                          pattern = { previousVersion, _ -> "Stable-$previousVersion-" },
+                          replacement = { currentVersion, _ -> "Stable-$currentVersion-" })
+        }
+    }
 }
 
+/**
+ * Helper method to make calling the fileUpdate action of the pre-release hoock of the Axion
+ * release plugin. Currently it is written in Groovy and uses Groovy closures which are a
+ * pain to use in Kotlin. They say they are working on changing to a more Kotlin friendly
+ * version and when they do we can remove this.
+ *
+ * @param file the name of the file to process.
+ * @param pattern a lambda returning the pattern to match.
+ * @param replacement a lambda returning the replacement string.
+ */
+fun HooksConfig.preFileUpdate(
+    file: String,
+    pattern: (String, pl.allegro.tech.build.axion.release.domain.hooks.HookContext) -> String,
+    replacement: (String, pl.allegro.tech.build.axion.release.domain.hooks.HookContext) -> String,
+) {
+    pre(
+        "fileUpdate", mapOf(
+            "file" to file, "pattern" to KotlinClosure2(pattern), "replacement" to KotlinClosure2(replacement)
+        )
+    )
+}
+
+/**
+ * Loads the given properties file and returns the Properties created from it.
+ */
 fun loadProperties(filename: String): Properties {
     val file = rootProject.file(filename)
     if (!file.exists()) {
